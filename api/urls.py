b2ea207cdd2d24a290510ec1122e67e7f0655f4d
joan aluka <mmbonojoan@gmail.com>
2019-07-18 17:48:40 +0300

    my heroku project

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from .views import StudentViewSet
+from .views import TeacherViewSet
+from .views import CourseViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("students",StudentViewSet)
+router.register("teachers",TeacherViewSet)
+router.register("course",CourseViewSet)
+
+
+urlpatterns=[
+path("",include(router.urls)),
+
+]
